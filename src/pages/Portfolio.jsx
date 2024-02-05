99f3519fcefaaefe0f4e4d18b903026c4529a728
Kunal Shah <you@example.com>
2024-02-05 17:21:22 -0500

    made some small tweaks

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,10 +2,10 @@ import "./Portfolio.css"
 import React from 'react';
 // importing the images for the projects to later be used 
 import mingo from "./assets/mingo.png";
-import textEditor from "./assets/texteditor.jpeg"
+import textEditor from "./assets/text.png"
 import noteTaker from "./assets/NoteTaker.png"
-import scheduler from "./assets/scheduler.png"
-import multipleChoice from "./assets/multchoice.png"
+import scheduler from "./assets/schedule.png"
+import multipleChoice from "./assets/quiz.jpeg"
 import password from "./assets/password.png"
 export default function Portfolio() {
 
@@ -13,42 +13,42 @@ export default function Portfolio() {
   const projects = [
     {
       title: 'Mingo Dating App',
-      description: 'A simplified dating app. For testing purposes use: janedoe@gmail.com, password:1234567',
+      description: 'This is a dating website where the user can connect with others, find events in the area, and find other users who enjoy the same hobbies. The goal is to get people outside and meeitng one another, than only online. For testing purposes use: janedoe@gmail.com, password:1234567',
       link: 'https://min-go-dating-app-617b3295d223.herokuapp.com/',
       image: mingo,
-      github: "https://github.com/bahossdev/Min-Go-Dating-App",
+      github: "https://github.com/unfazedxx/Min-Go-Dating-App",
     },
     {
       title: 'Text Editor',
-      description: 'A text editor which uses PWAs.',
+      description: 'A text editor which uses progressive web applications to allow for online and offline use. The user also has the ability to download a desktop version of the app.',
       link: 'https://text-editor-wpoy.onrender.com/ ',
       image: textEditor,
       github: "https://github.com/unfazedxx/Text_editor"
     },
     {
       title: 'Note Taker',
-      description: 'A note taking application which uses Express.js',
+      description: 'A note taking application using Express.js. This web app allows the user to create notes, save, as well as delete the note from their list.',
       link: 'https://note-taker-z2k7.onrender.com/notes',
       image: noteTaker,
       github: " https://github.com/unfazedxx/Note_Taking_App_ExpressJS",
     },
     {
       title: 'Daily Scheduler',
-      description: 'A text editor which uses PWAs.',
+      description: 'A daily task scheduler, for each hour of the day. The user can add, update and save their tasks based on the specific time, and have easy acecss to it on the go.',
       link: 'https://unfazedxx.github.io/daily_scheduler_/ ',
       image: scheduler,
       github: "https://github.com/unfazedxx/daily_scheduler"
     },
     {
       title: 'Coding Quiz',
-      description: 'A text editor which uses PWAs.',
+      description: 'A coding quiz which implements HTML, CSS and Javascript. This quiz has a count down timer, question prompts and an option to save your score using local storage.',
       link: ' https://unfazedxx.github.io/multiple-choice-quiz/ ',
       image: multipleChoice,
       github: "https://github.com/unfazedxx/multiple-choice-quiz"
     },
     {
       title: 'Password Generator',
-      description: 'A text editor which uses PWAs.',
+      description: 'A password generator application where the user is prompted for specifc items they would like to include in their password.',
       link: 'https://unfazedxx.github.io/Online_Random_Password_Generator/ ',
       image: password,
       github: "https://github.com/unfazedxx/Online_Random_Password_Generator"
